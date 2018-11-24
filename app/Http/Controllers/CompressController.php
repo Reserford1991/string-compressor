a285d1aafe4345718f4044c21e1d17a4b6ffb18e
Reserford1991 <vitaliymatveev1991@gmail.com>
2018-11-24 17:37:10 +0200

    Add compression algorythm

--- app/Http/Controllers/CompressController.php
+++ app/Http/Controllers/CompressController.php
@@ -12,9 +12,37 @@ class CompressController extends Controller
             $this->validateRequest($request, '/^[a-f]+$/u');
 
 
+            // String compression algorithm
+            $deComressed = $request->message;
+            $compressed = '';
+            $compressedArr = str_split($deComressed);
+            $i = 0;
+            $j = 0;
+            $tempArr = [];
+            $substringsArr[0] = $compressedArr[0];
+            foreach($compressedArr as $letter) {
+                if($i > 0) {
+                    if($compressedArr[$i-1] == $compressedArr[$i]) {
+                        $substringsArr[$j] .= $letter;
+                    } else {
+                        $j++;
+                        $substringsArr[$j] = $letter;
+                    }
+                }
+                $i++;
+            }
+            foreach ($substringsArr as $substring) {
+                $length = strlen($substring);
+                if ($length < 3) {
+                    $compressed .= $substring;
+                }
+                if ($length >= 3){
+                    $firstLetter = substr($substring, 0, 1);
+                    $compressed .= $firstLetter . $length;
+                }
+            }
 
-            $response = $this->returnResponse('success', $request->message);
-
+            $response = $this->returnResponse('success', $compressed);
             return response()->json($response);
         } catch (Exception $e) {
             $response = $this->returnResponse('error', 'Backend Validation went wrong!');
